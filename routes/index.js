7d0b1b865be63442922aaf6f4b6f32393ae85fe4
Rezaraihannn <rezaraihannn@student.telkomuniversity.ac.id>
2023-09-11 21:29:07 +0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var router = express.Router();
+const antaresApi = require("../service/antaresAPI");
+/* GET home page. */
+router.get("/", async function (req, res, next) {
+  let status = "mati";
+  // get data
+  const data = await antaresApi.getLatestData();
+  // parse data disini
+  const antaresData = JSON.parse(data["m2m:cin"].con)
+  let temperature = antaresData.temperature ?? 0
+  let humidity = antaresData.humidity ?? 0
+  
+
+  res.render("index", { status, temperature, humidity });
+});
+
+/* POST DATA */
+router.post("/postData", async function (req, res, next) {
+  const data = {
+    "m2m:cin": {
+      con: JSON.stringify(req.body),
+    }
+  };
+  await antaresApi.postData(data).then((response) => {
+    res.status(200).json(response.data);
+  });
+});
+
+module.exports = router;
